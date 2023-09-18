7b6119c8b097c216a0c05b70c96664aad4b4dcc3
Habidhossen <habidhossen2@gmail.com>
2023-09-19 01:59:54 +0600

    product model added

---
+++ models/product.model.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    productName: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    keyFeatures: [{ key: String, value: String }],
+    individualRating: {
+      type: Number,
+    },
+    averageRating: {
+      type: Number,
+    },
+    imageUrl: {
+      type: String,
+    },
+    reviews: [
+      {
+        reviewerName: {
+          type: String,
+        },
+        reviewerEmail: {
+          type: String,
+        },
+        reviewerImgUrl: {
+          type: String,
+        },
+        comments: {
+          type: String,
+        },
+        rating: {
+          type: Number,
+          min: 1,
+          max: 5,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Products", productSchema);
